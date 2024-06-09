359360713350293648b9f2930b1754439c7f862b
unset <109896207+kaydendua@users.noreply.github.com>
2024-06-09 23:55:24 +0800

    Add files via upload

---
+++ NavBar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import icon from "../assets/icon.jpg";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const location = useLocation();
+
+  const handleLinkClick = (link) => {
+    if (location.pathname !== link) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
+  const links = [
+    {
+      id: 1,
+      text: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      text: "Projects",
+      link: "/projects",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50 bg-opacity-80 backdrop-blur-sm">
+      <div className="flex items-center">
+        <img src={icon} alt="logo" className="w-14 h-14 rounded-full" />
+        <h1 className="ml-4 text-xl font-CenturyGothic">Kayden Dua</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, text, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            <Link
+              to={link}
+              smooth
+              duration={500}
+              onClick={() => handleLinkClick(link)}
+            >
+              {text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, text, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link
+                to={link}
+                smooth
+                duration={500}
+                onClick={() => handleLinkClick(link)}
+              >
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
